60d374b77055cc59f168a845e057169ab8c300c1
gibidon <malykhinig@gmail.com>
2023-11-01 12:08:26 +0700

    project settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { styled } from 'styled-components';
+
+const Div = styled.div`
+	color: green;
+	font-size: 50px;
+	text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			Hello <p>ksfdj</p>
+			<div>some line</div>
+			<i className="fa fa-camera-retro"></i>
+			<Div>
+				<i className="fa fa-camera-retro"></i>
+				<i
+					class="fa fa-user-circle"
+					aria-hidden="true"
+				></i>
+			</Div>
+		</div>
+	);
 }
 
 export default App;

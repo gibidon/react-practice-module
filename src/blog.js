7043839a6615d1f22182291e1bccaa4f293003c1
gibidon <malykhinig@gmail.com>
2023-11-04 21:37:29 +0700

    Реализовать шапку

--- src/blog.js
+++ src/blog.js
@@ -1,19 +1,29 @@
 import './App.css';
 import { styled } from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './components';
 
 const Content = styled.div`
 	padding: 120px 0;
 `;
 
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	background-color: #fff;
+	margin: 0 auto;
+`;
 const H2 = styled.h2`
 	text-aligh: center;
 `;
-const Header = () => <div>Шапка</div>;
+
 const Footer = () => <div>Подвал</div>;
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 
 			<Content>
@@ -51,7 +61,7 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
 
